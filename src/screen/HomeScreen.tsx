0144df83f2f3d1ca3ea9d57bb698e25e68be2a39
Antoine Emond <antoine.emond.96@gmail.com>
2022-02-20 17:44:57 -0500

    added map to android and ios

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Button } from '../main/components/Button';
 import Label from '../main/components/Label';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button label='Patate' onPress={() => {}} />
-      <Label variant='h1'>hello</Label>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
